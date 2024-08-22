924419610c06ae270cb35782bea95b6cd69ae20b
Tufan10 <148412603+Black44r@users.noreply.github.com>
2024-08-22 14:55:54 +0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevents the default form submission action
+
+    var password = document.getElementById('password').value;
+    var correctPassword = "Termux@10"; // Replace "yourpassword" with your predefined password
+
+    if (password === correctPassword) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerText = 'Login successful!';
+        window.location.href = "https://black44r.github.io/tufan.github.io/"; // Redirect to Google.com
+    } else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerText = 'Incorrect password!';
+    }
+});
